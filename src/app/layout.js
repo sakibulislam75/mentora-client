d93bcbb35767705fff522e4233bbc49698359368
sakibulislam75 <sakibulislam1623@gmail.com>
2026-09-18 19:16:41 +0600

    feat: add navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { MainNavbar } from '@/components/NavBar';
 
 const geistSans = Geist({
    variable: '--font-geist-sans',
@@ -19,7 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
    return (
       <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-         <body className="min-h-full flex flex-col">{children}</body>
+         <body className="min-h-full flex flex-col">
+            <MainNavbar />
+            {children}
+         </body>
       </html>
    );
 }
